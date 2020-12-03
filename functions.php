40861ac5ac56b495d165672826ac62214a93a858
Vladimir <fayst_@bk.ru>
2020-12-03 14:57:19 +0300

    Реализовал возможность динамически менять логотип как в хедере так и в футоре через админ панель.

--- functions.php
+++ functions.php
@@ -5,4 +5,6 @@
     wp_enqueue_style('childhood-style', get_stylesheet_uri());
     wp_enqueue_script('childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
   }
+
+  add_theme_support( 'custom-logo' );
 ?>
